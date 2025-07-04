1fb4d276186f6258da4bf8d4a9a2110958615e00
Jeronimo Corigliano <82349162+Krilzs@users.noreply.github.com>
2025-07-03 21:35:29 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
 );
