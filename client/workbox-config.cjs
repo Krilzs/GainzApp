0f1c30eea32cafb51b255e71612b6922e98d526e
Jeronimo Corigliano <jerofc143@gmail.com>
2025-07-13 02:46:02 -0300

    Adding PWAinstall settings

--- client/workbox-config.cjs
+++ client/workbox-config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   globDirectory: "dist/",
   globPatterns: ["**/*.{js,css,html,png,svg,woff2}"],
-  swSrc: "service-worker.js",
+  swSrc: "public/service-worker.js",
   swDest: "dist/service-worker.js",
   maximumFileSizeToCacheInBytes: 5000000, // 5MB
 };
